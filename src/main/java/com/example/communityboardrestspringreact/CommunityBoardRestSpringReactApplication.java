1e7746e06cf35fc263342e70ff43da128c1d3e5b
leejongwoo <leejongwoo.dev@gmail.com>
2022-07-24 20:29:51 +0900

    Created Project

---
+++ src/main/java/com/example/communityboardrestspringreact/CommunityBoardRestSpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.communityboardrestspringreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityBoardRestSpringReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityBoardRestSpringReactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/communityboardrestspringreact/CommunityBoardRestSpringReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.communityboardrestspringreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityBoardRestSpringReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

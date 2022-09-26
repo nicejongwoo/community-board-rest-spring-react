b8e0ce28829f173ac4570dfbc0d15771550ace12
leejongwoo <leejongwoo.dev@gmail.com>
2022-09-26 22:38:46 +0900

    Update test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,6 +8,13 @@ INSERT INTO roles (name) VALUES('ROLE_USER');
 
 INSERT INTO account (email, name, password) VALUES ('user@email.com', '사용자', '{noop}password');
 INSERT INTO account_roles (account_id, role_id) VALUES (1, 3);
+INSERT INTO account (email, name, password) VALUES ('manager@email.com', '매니저', '{noop}password');
+INSERT INTO account_roles (account_id, role_id) VALUES (2, 2);
+INSERT INTO account_roles (account_id, role_id) VALUES (2, 3);
+INSERT INTO account (email, name, password) VALUES ('admin@email.com', '관리자', '{noop}password');
+INSERT INTO account_roles (account_id, role_id) VALUES (3, 1);
+INSERT INTO account_roles (account_id, role_id) VALUES (3, 2);
+INSERT INTO account_roles (account_id, role_id) VALUES (3, 3);
 
 INSERT INTO community (created_by, created_at, updated_by, updated_at, title, category_id, content) VALUES ('user@email.com', CURRENT_TIMESTAMP, 'user@email.com', CURRENT_TIMESTAMP, '영상에 관한 질문입니다.', 1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Volutpat odio facilisis mauris sit amet massa. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Mi eget mauris pharetra et. Non tellus orci ac auctor augue. Elit at imperdiet dui accumsan sit. Ornare arcu dui vivamus arcu felis. Egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst quisque sagittis purus. Pulvinar elementum integer enim neque volutpat ac.');
 

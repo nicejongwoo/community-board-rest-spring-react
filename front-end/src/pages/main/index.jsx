0cca399678759825712829a750c83c5e8e6b865b
leejongwoo <leejongwoo.dev@gmail.com>
2022-09-26 22:37:20 +0900

    Created main page

---
+++ front-end/src/pages/main/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StyledSection} from "App";
+
+const Main = () => {
+    return (
+        <StyledSection className="section">
+
+            <div className="content-wrapper">
+            </div>
+
+        </StyledSection>
+    );
+};
+
+export default Main;

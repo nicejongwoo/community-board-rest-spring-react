016f83ae74c4ea79426159c083ebd1c059a8e29b
leejongwoo <leejongwoo.dev@gmail.com>
2022-10-28 23:52:39 +0900

    Implements AccountSearchResponse class

--- src/main/java/com/example/communityboardrestspringreact/web/dto/response/account/AccountSearchResponse.java
+++ src/main/java/com/example/communityboardrestspringreact/web/dto/response/account/AccountSearchResponse.java
@@ -1,4 +1,26 @@
 package com.example.communityboardrestspringreact.web.dto.response.account;
 
+import com.example.communityboardrestspringreact.web.dto.response.role.RoleCodeResponse;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Getter
+@Setter
 public class AccountSearchResponse {
+
+    private int rowNum;
+    private Long id;
+    private String accountToken;
+    private String name;
+    private String email;
+    private String phone;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime joinedAt;
+    private String profileImage;
+    private Set<RoleCodeResponse> roles;
+
 }
